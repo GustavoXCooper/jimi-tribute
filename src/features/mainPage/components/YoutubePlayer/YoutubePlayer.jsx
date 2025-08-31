68006393a6c2130edcaf48e3d48bf712ec98553c
Gustavo <gustavoxcooper@gmail.com>
2025-08-31 10:56:40 -0300

    feature: add youtube player (mocked)

--- src/features/mainPage/components/YoutubePlayer/YoutubePlayer.jsx
+++ src/features/mainPage/components/YoutubePlayer/YoutubePlayer.jsx
@@ -1,9 +1,16 @@
-import './provisorio.css'
+import ReactPlayer from 'react-player'
 
 export const YoutubePlayer = () => {
+    const baseURL = 'https://www.youtube.com/watch?v='
     return (
         <>
-            <div> teste </div>
+            <ReactPlayer 
+                src= { baseURL + "ODhMdujZeEY"}
+                style={{
+                    width: '100px',
+                    
+                }}
+            />
         </>
     )
 }
